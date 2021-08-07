495fa279b44b542eb4a4f79c6936805611d60aaa
RYOTA2528 <1216022@gmail.com>
2021-08-07 12:06:38 +0000

    [Add]adminモデルとitemsモデルのアソシエーション作成

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,4 +1,22 @@
 class Admin::ItemsController < ApplicationController
+  
   def new
- end
+    @item = Item.new
+  end
+  
+  def create
+    
+  end
+  
+  def index
+  end
+  
+  def show
+  end
+  
+  def edit
+  end
+  
+  def update
+  end
 end
