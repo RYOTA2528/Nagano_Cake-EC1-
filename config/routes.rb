40b6779364ec319705bf1c1a6e0bb82ca9c72501
RYOTA2528 <1216022@gmail.com>
2021-08-07 01:16:05 +0000

    [Add]deviseのAdminモデル追加&namespaceによるadmin/homesコントローラー・topアクション

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  namespace :admin do
+  devise_for :admins
+  root to: 'admin/homes#top'
+  resources :items
+end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
