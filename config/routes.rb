47c4f74fa5fe3024d88c14c47b0288e2a0fc1662
RYOTA2528 <1216022@gmail.com>
2021-08-11 08:54:21 +0000

    [Update]Admin並びにPublicのroutes編集・各ログインページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,39 @@
 Rails.application.routes.draw do
 
+devise_for :customers, controllers: {
+  sessions:      'public/sessions',
+  passwords:     'public/passwords',
+  registrations: 'public/registrations'
+}
+devise_for :admins, path: "/admin", controllers: {
+  sessions:      'admin/sessions',
+  passwords:     'admin/passwords',
+  registrations: 'admin/registrations'
+}
 
-  scope module: :public do
-  devise_for :customers
-  end
 
-  # scope module: :admin do
-  # devise_for :admins
-  # end
 
-devise_for :admins
-# controllers: {
-#   sessions:      'admins/sessions',
-#   passwords:     'admins/passwords',
-#   registrations: 'admins/registrations'
-# }
-  namespace :admin do
-  root to: 'homes#top'
-  resources :items
-  resources :genres
-  # 余裕あればgenresはonlyの指定を行う！
+
+scope module: :public do
+
 end
+
+
+
+
+# devise_scope :admin do
+#   get 'admin/sign_in' => 'admin/sessions#new'
+#   post '/admin/sign_in' => 'admin/sessions#create'
+#   delete  '/admin/sign_out' => 'admin/sessions#destroy'
+# end
+
+  namespace :admin do
+
+    root to: 'homes#top'
+    resources :items
+    resources :genres
+   # 余裕あればgenresはonlyの指定を行う！
+
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
